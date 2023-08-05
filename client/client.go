fabc050558010e4f1cc1101ed190e8d945872ff6
half-life666 <half-life@jibudata.com>
2023-08-05 22:53:07 +0800

    fix import and mod tidy

--- client/client.go
+++ client/client.go
@@ -8,7 +8,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 
-	pb "github.com/jibudata/archival-api"
+	pb "github.com/jibudata/archive-api"
 )
 
 type ArchivalClient struct {
